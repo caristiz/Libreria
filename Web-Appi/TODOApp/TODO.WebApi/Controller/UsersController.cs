a324b2514a70ce3168990ed5dd42657e94a19ec2
caristiz <catalinaaristizabal312613@correo.itm.edu.co>
2023-10-03 00:38:08 -0500

    First commit

---
+++ Web-Appi/TODOApp/TODO.WebApi/Controller/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TODO.WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        public UsersController() { }
+
+        [HttpGet]
+        public async Task <IActionResult> GetHelloWorld()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
